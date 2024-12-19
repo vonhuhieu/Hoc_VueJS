4df93c20ca164930fb458d2f38a4c1b3caed670f
Hieu Vo <hieu.vo@sapotacorp.vn>
2024-12-20 04:34:53 +0700

    Tìm hiểu về computed, thay vì gọi hàm trong template, dùng computed để biến hàm đó thành biến, tránh phải gọi hàm

---
+++ 04-computed/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { computed, reactive } from 'vue';
+
+const book = reactive({
+  name: "USSR",
+  chapters: [],
+});
+
+const addChapter = () => {
+  book.chapters.push(1);
+};
+
+const isPublished = computed(() => {
+  console.log("check ham isPublished");
+  return book.chapters.length > 0 ? "Published" : "not published";
+});
+</script>
+
+<template>
+  hello world
+  <p>Book's name: {{ book.name }}</p>
+  <p>Status: {{ isPublished }}</p>
+  <button @click="addChapter">Add chapter</button>
+</template>
