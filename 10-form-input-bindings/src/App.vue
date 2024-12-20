72404c45f3ca40f2291d3735b573ad0a4b162efd
Hieu Vo <hieu.vo@sapotacorp.vn>
2024-12-20 11:23:41 +0700

    v-model

---
+++ 10-form-input-bindings/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue';
+
+const text = ref('hello');
+const multilineText = ref("");
+
+</script>
+
+<template>
+  <div>
+    <h1>hello world</h1>
+    <p>Text: {{ text }}</p>
+    <input v-model="text" />
+    <br/>
+
+    <p>Multiline text: {{ multilineText }}</p>
+    <textarea v-model="multilineText" placeholder="Input your message"/>
+  </div>
+</template>
+
+<style></style>
