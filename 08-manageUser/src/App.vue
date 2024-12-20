48cdcbefc380b70590cc8b19a4a13142f7a25325
Hieu Vo <hieu.vo@sapotacorp.vn>
2024-12-20 10:29:03 +0700

    thuc hanh voi 2 bai tap don gian

---
+++ 08-manageUser/src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { ref } from 'vue';
+
+const newUser = ref('');
+
+const listUsers = ref([]);
+
+const addNewUser = () => {
+  if (newUser.value.trim() === '') {
+    return;
+  }
+  const user = {
+    id: listUsers.value.length + 1,
+    name: newUser.value,
+  };
+  listUsers.value.push(user);
+  newUser.value = '';
+};
+
+const deleteUser = (id) => {
+  listUsers.value = listUsers.value.filter(user => user.id != id);
+};
+</script>
+
+<template>
+  <div>
+    Hello world
+    <input type="text" v-model="newUser" @keyup.enter="addNewUser" />
+    <button @click="addNewUser">Add</button>
+    <ul>
+      <li v-for="user in listUsers" :key="user.id">
+        {{ user.id }} : {{ user.name }}
+        <button @click="deleteUser(user.id)">Delete</button>
+      </li>
+    </ul>
+  </div>
+</template>
---
+++ 08-toDoApp/src/App.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { ref } from 'vue';
+  const newToDo = ref('');
+  const toDos = ref([
+    {
+      id: 1,
+      text: "Learn VueJS",
+    },
+    {
+      id: 2,
+      text: "Build a toDo app",
+    }
+  ]);
+  const addToDo = () => {
+    if(newToDo.value.trim() === '') return
+    const toDo = {
+      id: toDos.value.length + 1,
+      text: newToDo.value,
+    };
+    toDos.value.push(toDo);
+    newToDo.value = "";
+  };
+  const deleteToDo = (id) => {
+    toDos.value = toDos.value.filter(toDo => toDo.id != id);
+  };
+</script>
+
+<template>
+  <div>
+    <h1>hello world</h1>
+    <input type="text" v-model="newToDo" @keyup.enter="addToDo"/>
+    <button @click="addToDo">Add</button>
+    <ul>
+      <li v-for="toDo in toDos" :key="toDo.id">
+        {{ toDo.text }}
+        <button @click="deleteToDo(toDo.id)">Delete</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style></style>
