58d5fa6a7e404a4f03599c4369546cf9a8ddd196
Hieu Vo <hieu.vo@sapotacorp.vn>
2024-12-20 16:24:55 +0700

    cac loai source cua watch

---
+++ 12-watchers-2/src/App.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { ref, watch } from 'vue';
+
+const x = ref(0);
+const y = ref(0);
+
+const user = ref({
+  age: 18,
+});
+
+watch(
+  () => x.value + y.value,
+  (sum) => {
+    // console.log(`Tong cua x va y: ${sum}`);
+  }
+);
+
+watch([x, () => y.value + 1], ([newX, newY]) => {
+  console.log(`Gia tri cap nhat: ${newX}, ${newY}`);
+});
+
+watch(() => user.value.age, (newTuoi) => {
+  console.log(`Tuoi: ${newTuoi}`);
+});
+
+const changeAge = () => {
+  user.value.age = 20;
+};
+
+const increment = () => {
+  x.value++;
+  y.value++;
+}
+</script>
+
+<template>
+  <div>
+    <h1>hello world</h1>
+    <p>{{ x }}, {{ y }}</p>
+    <button @click="increment">increase</button>
+    <br />
+    <button @click="changeAge">change Age</button>
+    <p>{{ user.age }}</p>
+  </div>
+</template>
+
+<style></style>
