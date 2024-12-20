2942cd70678e267cc5c7103f3f68598850fe4e72
Hieu Vo <hieu.vo@sapotacorp.vn>
2024-12-20 14:50:14 +0700

    lam quen voi onMounted va onUnmounted

---
+++ 11-lifecycle-hooks/src/views/AboutView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <h1>Timer: {{ time }}</h1>
+  </div>
+</template>
+
+<script setup>
+import { onMounted, onUnmounted, ref } from 'vue';
+
+const time = ref(0);
+let intervalId = null;
+
+onMounted(() => {
+  // cú pháp cơ bản của setInterval: const intervalId = setInterval(callback, delay);
+  // callback: thực thi sau khoảng thời gian chỉ định
+  // delay: khoảng thời gian (ms) giữa các lần thực thi
+  intervalId = setInterval(() => {
+    time.value++
+  }, 1000)
+});
+
+onUnmounted(() => {
+  clearInterval(intervalId);
+});
+</script>
---
+++ 11-lifecycle-hooks/src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <h1>This is a home page</h1>
+    <h1>Countdown: {{ time }}</h1>
+  </div>
+</template>
+
+<script setup>
+import { onMounted, onUnmounted, ref } from 'vue';
+
+const time = ref(60);
+
+let intervalId = null;
+
+onMounted(() => {
+  intervalId = setInterval(() => {
+    time.value--;
+  }, 1000);
+});
+
+onUnmounted(() => {
+  clearInterval(intervalId);
+});
+</script>
