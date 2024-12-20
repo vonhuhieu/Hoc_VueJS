45ab72208a03cbc78a204c27da0064d8c70eb3d0
Hieu Vo <hieu.vo@sapotacorp.vn>
2024-12-20 10:39:50 +0700

    event handling

---
+++ 09-eventHandling/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+import { ref } from 'vue';
+
+const count = ref(0);
+
+const greet = (event) => {
+  console.log(event.target);
+  alert(`Xin chao ${event.type}`);
+};
+
+const sayHi = (name) => {
+  alert(`Xin chao ${name}`);
+};
+</script>
+
+<template>
+  <div>
+    <h1>hello world</h1>
+    <button @click="greet">Them 1</button>
+    <button @click="sayHi('a')">Say Hi</button>
+    <p>{{ count }}</p>
+  </div>
+</template>
+
+<style></style>
