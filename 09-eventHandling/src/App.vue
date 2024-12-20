d263c2786924475869c077a80f33cd407a2caece
Hieu Vo <hieu.vo@sapotacorp.vn>
2024-12-20 10:49:01 +0700

    tim hieu mot so doi so event

--- 09-eventHandling/src/App.vue
+++ 09-eventHandling/src/App.vue
@@ -1,25 +1,30 @@
 <script setup>
 
-import { ref } from 'vue';
-
-const count = ref(0);
-
-const greet = (event) => {
-  console.log(event.target);
-  alert(`Xin chao ${event.type}`);
+const sayHi1 = () => {
+  alert(`Xin chao 1`)
 };
 
-const sayHi = (name) => {
-  alert(`Xin chao ${name}`);
+const sayHi2 = () => {
+  alert(`Xin chao 2`);
+};
+
+const submit = () => {
+  alert("form submitted");
 };
 </script>
 
 <template>
   <div>
     <h1>hello world</h1>
-    <button @click="greet">Them 1</button>
-    <button @click="sayHi('a')">Say Hi</button>
-    <p>{{ count }}</p>
+    <!-- click vao nut 2 se chi goi ham tai nut 2, khong anh huong den nut 1 -->
+    <button @click.self="sayHi1">
+      <button @click.self="sayHi2">say hi 2</button>
+      say hi 1
+    </button>
+    <!-- <form @submit.prevent.stop="submit">
+      <button type="submit">submit</button>
+    </form> -->
+
   </div>
 </template>
 
