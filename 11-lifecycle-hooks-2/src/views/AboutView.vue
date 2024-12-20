26a9186dabc702ad1ae8887668abb6ab82969945
Hieu Vo <hieu.vo@sapotacorp.vn>
2024-12-20 15:24:12 +0700

    onBeforeUpdated va onUpdated

---
+++ 11-lifecycle-hooks-2/src/views/AboutView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <h1 id="count">Timer: {{ count }}</h1>
+    <button @click="increment">Increase</button>
+  </div>
+</template>
+
+<script setup>
+import { onBeforeUpdate, onUpdated, ref } from 'vue';
+const count = ref(0);
+
+const increment = () => {
+  count.value++;
+};
+
+onBeforeUpdate(() => {
+  console.log("onBeforeUpdate",document.getElementById("count").textContent);
+});
+
+onUpdated(() => {
+  console.log("onUpdate",document.getElementById("count").textContent);
+});
+</script>
