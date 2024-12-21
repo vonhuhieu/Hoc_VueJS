71281c99b7284e3b495d99895e5eb89a3b366f8f
Hieu Vo <hieu.vo@sapotacorp.vn>
2024-12-21 09:37:38 +0700

    componentEvents

---
+++ 15-componentEvents/src/App.vue
@@ -0,0 +1,97 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router';
+import HelloWorld from './components/HelloWorld.vue';
+import { ref } from 'vue';
+import ButtonComponent from './components/ButtonComponent.vue';
+
+const count = ref(0);
+
+const increaseBy = (number) => {
+  count.value = count.value + number;
+};
+
+const increaseBy2 = (number1, number2) => {
+  count.value = count.value + number1 + number2;
+};
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld v-bind:msg="count" />
+      <ButtonComponent @increase="count++" @increaseByTwoTimes="count = count + 2" @increaseBy="increaseBy" @increaseBy2="increaseBy2"/>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
---
+++ 15-componentEvents/src/components/ButtonComponent.vue
@@ -0,0 +1,22 @@
+<template>
+    <button @click="callFunctionIncrease">Click</button>
+    <button @click="callFunctionIncreaseByTwoTimes">Click me</button>
+    <button @click="callFunctionIncreaseBy">click me 2</button>
+    <button @click="callFunctionIncreaseBy2">click me 3</button>
+</template>
+
+<script setup>
+const emit = defineEmits(['increase', 'increaseByTwoTimes', 'increaseBy', 'increaseBy2']);
+const callFunctionIncrease = () => {
+    emit('increase');
+}; 
+const callFunctionIncreaseByTwoTimes = () => {
+    emit('increaseByTwoTimes')
+};
+const callFunctionIncreaseBy = () => {
+    emit('increaseBy', 5);
+};
+const callFunctionIncreaseBy2 = () => {
+    emit('increaseBy2', 15,20);
+};
+</script>
