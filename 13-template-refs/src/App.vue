56b53d939ac6cd7fab28317b7e038ce253c8bce8
Hieu Vo <hieu.vo@sapotacorp.vn>
2024-12-21 07:48:13 +0700

    refs trong v-for

--- 13-template-refs/src/App.vue
+++ 13-template-refs/src/App.vue
@@ -2,9 +2,17 @@
 import { onMounted, ref } from 'vue';
 
 const inputRef = ref(null);
+const list = ref(["item 1", "item 2", "item 3"]);
+const itemRefs = ref([]);
 onMounted(() => {
   inputRef.value.focus();
+
+  itemRefs.value.forEach((item) => {
+    item.style.transition = 'opacity 10s'
+    item.style.opacity = 1;
+  });
 });
+console.log(itemRefs);
 console.log(inputRef);
 </script>
 
@@ -12,6 +20,11 @@ console.log(inputRef);
   <div>
     <h1>hello world</h1>
     <input ref="inputRef"/>
+    <ul>
+      <li v-for="(item,index) in list" :key="index" ref="itemRefs" style="opacity: 0;">
+        {{ item }}
+      </li>
+    </ul>
   </div>
 </template>
 
