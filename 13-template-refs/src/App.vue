cfe2d6aa102af9b754db3103a7683432257bc58d
Hieu Vo <hieu.vo@sapotacorp.vn>
2024-12-21 07:55:38 +0700

    refs kieu ham

--- 13-template-refs/src/App.vue
+++ 13-template-refs/src/App.vue
@@ -5,15 +5,18 @@ const inputRef = ref(null);
 const list = ref(["item 1", "item 2", "item 3"]);
 const itemRefs = ref([]);
 onMounted(() => {
-  inputRef.value.focus();
-
+  // inputRef.value.focus();
+  console.log(itemRefs.value);
   itemRefs.value.forEach((item) => {
     item.style.transition = 'opacity 10s'
     item.style.opacity = 1;
   });
 });
-console.log(itemRefs);
-console.log(inputRef);
+// console.log(itemRefs);
+// console.log(inputRef);
+const initializeThirdPartyLibrary = (el) => {
+  console.log(el);
+}
 </script>
 
 <template>
@@ -25,6 +28,10 @@ console.log(inputRef);
         {{ item }}
       </li>
     </ul>
+
+    <input v-bind:ref="(el) => {
+      initializeThirdPartyLibrary(el)
+    }"/>
   </div>
 </template>
 
