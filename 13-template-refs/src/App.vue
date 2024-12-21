1c2c5b3f94ac3efbe1fb89a9ec37d6af7cbcf85f
Hieu Vo <hieu.vo@sapotacorp.vn>
2024-12-21 07:41:22 +0700

    template-refs-> ap dung vi du khi vao mot trang, con tro se tu dong tro vao o input co thuoc tinh ref

---
+++ 13-template-refs/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+
+const inputRef = ref(null);
+onMounted(() => {
+  inputRef.value.focus();
+});
+console.log(inputRef);
+</script>
+
+<template>
+  <div>
+    <h1>hello world</h1>
+    <input ref="inputRef"/>
+  </div>
+</template>
+
+<style></style>
