4cfaf6e3f0aecfa2c0e168dab9c5995099f566e0
Hieu Vo <hieu.vo@sapotacorp.vn>
2024-12-21 09:56:42 +0700

    globalComponents

---
+++ 16-globalComponents/src/main.js
@@ -0,0 +1,16 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+import GlobalComponent from './components/GlobalComponent.vue'
+
+const app = createApp(App)
+
+app.component('ComponentA', GlobalComponent)
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
