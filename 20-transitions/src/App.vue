8d2b991fedee8a162195dfd3625f8bdd9d390f6b
Hieu Vo <hieu.vo@sapotacorp.vn>
2024-12-22 11:07:15 +0700

    transition group

--- 20-transitions/src/App.vue
+++ 20-transitions/src/App.vue
@@ -1,7 +1,19 @@
 <script setup>
-import { ref } from 'vue';
+import { reactive, ref } from 'vue';
 
 const isShow = ref(true);
+
+const items = reactive([1, 2, 3, 4, 5, 6, 7]);
+
+const addRandom = () => {
+  const randomNumber = Math.floor(Math.random() * 10 + 1);
+  items.push(randomNumber);
+};
+
+const removeRandom = () => {
+  const randomIndex = Math.floor(Math.random() * items.length);
+  items.value = items.splice(randomIndex, 1);
+};
 </script>
 
 <template>
@@ -9,7 +21,11 @@ const isShow = ref(true);
     <h1>hello world</h1>
     <h1>Transition</h1>
     <button @click="isShow = !isShow">Toggle fade</button>
-
+    <button @click="addRandom">Add random number</button>
+    <button @click="removeRandom">Remove</button>
+    <TransitionGroup name="list" tag="ul">
+      <li v-for="item in items" :key="item">{{ item }}</li>
+    </TransitionGroup>
     <Transition name="fade">
       <p v-if="isShow">Hello</p>
     </Transition>
@@ -25,5 +41,16 @@ const isShow = ref(true);
 .fade-enter-from,
 .fade-leave-to {
   opacity: 0;
+}
+
+.list-enter-active,
+.list-leave-active {
+  transition: all 0.5s ease;
+}
+
+.list-enter-from,
+.list-enter-to {
+  opacity: 0;
+  transition: translateX(30px);
 }
 </style>
