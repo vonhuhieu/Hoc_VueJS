cdfcc1540cd1eeed05e48cddabbaa359e1699f87
Hieu Vo <hieu.vo@sapotacorp.vn>
2024-12-22 10:20:28 +0700

    cac hook cua directives

--- 18-customDirectives/src/App.vue
+++ 18-customDirectives/src/App.vue
@@ -1,11 +1,26 @@
 <script setup>
+const vColor = {
+  mounted: (el, binding) => {
+    el.style.color = binding.value
+  },
+};
 
+const vDemo = {
+  mounted: (el, binding) => {
+    el.style.color = binding.value.color,
+    el.style.background = binding.value.background
+  }
+};
 </script>
 
 <template>
   <div>
     <h1>hello world</h1>
-    <input v-focus placeholder="Input your name"/>
+    <h1 v-demo="{
+      color: 'red',
+      background: 'blue'
+    }">Custom Directives</h1>
+    <input v-focus placeholder="Input your name" />
   </div>
 </template>
 
