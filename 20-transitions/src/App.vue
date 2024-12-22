978f475208fd1e1cc322f90064aea6429d4d1b1f
Hieu Vo <hieu.vo@sapotacorp.vn>
2024-12-22 10:43:12 +0700

    transition

---
+++ 20-transitions/src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from 'vue';
+
+const isShow = ref(true);
+</script>
+
+<template>
+  <div>
+    <h1>hello world</h1>
+    <h1>Transition</h1>
+    <button @click="isShow = !isShow">Toggle fade</button>
+
+    <Transition>
+      <p v-if="isShow">Hello</p>
+    </Transition>
+  </div>
+</template>
+
+<style>
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 3s ease;
+}
+
+.v-enter-from,
+.v-enter-to {
+  opacity: 0;
+}
+</style>
