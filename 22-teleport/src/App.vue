3e40cc005805443ac9d0a4ce7ee5e8215f5f541d
Hieu Vo <hieu.vo@sapotacorp.vn>
2024-12-22 18:20:39 +0700

    teleport

---
+++ 22-teleport/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import MyModal from './components/MyModal.vue';
+
+
+</script>
+
+<template>
+  <div>
+    <h1>hello world</h1>
+    <MyModal/>
+  </div>
+</template>
+
+<style></style>
