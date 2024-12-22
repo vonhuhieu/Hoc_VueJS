7fe95466c9f4276b704e9bfb5df1b04d28ad4d95
Hieu Vo <hieu.vo@sapotacorp.vn>
2024-12-22 17:56:07 +0700

    component dong voi the component

---
+++ 21-dynamicComponents/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from 'vue';
+import ComponentA from './components/ComponentA.vue';
+import ComponentB from './components/ComponentB.vue';
+const currentComponent = ref(ComponentA);
+</script>
+
+<template>
+  <div>
+    <h1>hello world</h1>
+    <button @click="currentComponent = ComponentA">Show component A</button>
+    <button @click="currentComponent = ComponentB">Show component B</button>
+    <!-- <ComponentA /> -->
+    <component v-bind:is="currentComponent"></component>
+  </div>
+</template>
+
+<style></style>
---
+++ 21-dynamicComponents/src/components/ComponentB.vue
@@ -0,0 +1,6 @@
+<script setup>
+
+</script>
+<template>
+    <p>Component B</p>
+</template>
