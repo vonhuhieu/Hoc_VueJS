f4283bb5a25ac088f9da63f835ef89dfc81ee44a
Hieu Vo <hieu.vo@sapotacorp.vn>
2024-12-22 09:32:29 +0700

    composables

---
+++ 17-composables/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { useCounter } from './composables/useCounter';
+import { useMouse } from './composables/useMouse';
+
+const { count, increment, decrement } = useCounter();
+const { x, y } = useMouse();
+</script>
+
+<template>
+  <div>
+    <h1>hello world</h1>
+    <p>{{ count }}</p>
+    <button @click="increment">Increment</button>
+    <button @click="decrement">Decrement</button>
+
+    <p>X: {{ x }}, Y: {{ y }}</p>
+  </div>
+</template>
+
+<style></style>
