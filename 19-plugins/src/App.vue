c48b026dbc3b3dd42340f5e684f562b9abd09968
Hieu Vo <hieu.vo@sapotacorp.vn>
2024-12-22 10:32:27 +0700

    plugins

---
+++ 19-plugins/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <h1>hello world</h1>
+    <p>{{ $translate('greetings.hello') }}</p>
+    <p>{{ $translate('greetings.message') }}</p>
+  </div>
+</template>
+
+<style></style>
---
+++ 19-plugins/src/plugins/i18n.js
@@ -0,0 +1,11 @@
+export default {
+    install:(app, options) => {
+        app.config.globalProperties.$translate = (key) => {
+            return key.split(".").reduce((o , i) => {
+                if(o){
+                    return o[i]
+                }
+            }, options);
+        };
+    }
+}
